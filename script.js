f4f2452412e0b1603fb7fb55c458c2eafafe5c2b
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-01 20:13:02 +0530

    remove almost all alerts and added a n empty div in html

--- script.js
+++ script.js
@@ -3,27 +3,31 @@ document.getElementById("img-2").disabled = true;
 document.getElementById("img-3").disabled = true;
 document.getElementById("img-4").disabled = true;
 
+let msg = document.getElementById('msgDispaly');
+
 // array to store input info like name, user name, and email
 let formInfoArray = [];
-
-// array to store the score of dice onclick function
-let diceScore = [];
 
 // global variables to keep record of attempts for image 3 on click function and dice click for dice image
 let attempt = 2;
 let RemainingDiceClick = 3;
 let totalScore = 0;
+// functions--------------------------------------------------------------------------------------------------
 
 // Image 1 Function : Open form
 function openFormFunction() {
     document.getElementById('form').classList.remove('hidden');
 }
 
+//-----------------------------------------------------------------------------------------------------------
+
 // Form submit Function : Take inputs from user and submit the form with storing the info in global array
 function submitFormInfoFunction() {
     let name = document.getElementById('name').value;
     let userName = document.getElementById('userName').value;
     let email = document.getElementById('email').value;
+    const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
     if (name && userName && email) {
         let obj = {
             Name: name,
@@ -34,33 +38,41 @@ function submitFormInfoFunction() {
         formInfoArray.push(obj);
         document.getElementById("img-2").disabled = false;
         document.getElementById("img-1").disabled = true;
-        alert('Your Info is Submitted successfully! Click on the next image to view your info.')
+        msg.innerText = 'Your Info is Submitted successfully! Click on the next image to view your info.'
         document.getElementById('form').classList.add('hidden');
-        // console.log(formInfoArray);
     } else {
         alert(`Please fill all the fields.`)
     }
 }
 
+//---------------------------------------------------------------------------------------------------------------
 
 // Image 2 Function : Display form info 
 function displayFormInfoFunction() {
+    msg.classList.add('hidden');
     document.getElementById('info').innerText = `Name: ${formInfoArray[0].Name}, User Name: ${formInfoArray[0].UserName}, Email: ${formInfoArray[0].Email}`
     document.getElementById('displayInfo').classList.remove('hidden');
     document.getElementById("img-3").disabled = false;
     document.getElementById("img-2").disabled = true;
 }
 
+//-------------------------------------------------------------------------------------------------------------
+
 // Image 3 Function : Display dice image with attempt no
 function displayDiceWthScore() {
+    RemainingDiceClick = 3;
+    totalScore = 0;
+    document.getElementById('diceClicks').innerText = RemainingDiceClick;
+    document.getElementById('score').innerText = totalScore;
     document.getElementById('displayInfo').classList.add('hidden');
     attempt--;
     console.log(`attempt remaining= ${attempt}`);
     document.getElementById('attemptsLeft').innerText = attempt;
     document.getElementById('diceWthScore').classList.remove('hidden');
     document.getElementById("img-3").disabled = true;
-
 }
+
+//-------------------------------------------------------------------------------------------------------------
 
 // DiceClick function: this function keeps track of total score. If TS > 10 after 3 dice clicks it should enabled 4th image else alert to click on 3rd image
 
@@ -70,20 +82,17 @@ function diceClick() {
     let randomNo = Math.floor(Math.random() * 6) + 1;
     totalScore += randomNo;
     document.getElementById('score').innerText = totalScore;
-    // console.log(`RemainingDiceClick = ${RemainingDiceClick}`);
-    // console.log(`randomNo generated is   ${randomNo}`);
-    // console.log(`totalScore = ${totalScore}`);
     if (RemainingDiceClick === 0) {
         document.getElementById("dice-Image").disabled = true;
         document.getElementById('score').innerText = totalScore;
         if (totalScore > 10) {
-            alert(`Yehhh Congratulations 🎉🎉🎊🎊😊😊!!! Your total score is ${totalScore} which is greater than 10. Click on next Image to avail your coupon code.`);
+            msg.classList.remove('hidden');
+            msg.innerHTML = `Yehhh Congratulations 🎉🎉🎊🎊😊😊!!! Your total score is ${totalScore} which is greater than 10. Click on next Image to avail your coupon code.`
             document.getElementById('diceWthScore').classList.add('hidden');
             document.getElementById("img-4").disabled = false;
         } else {
             if (attempt != 0) {
-                // document.getElementById('score').innerText = totalScore;
-                alert(`O Ohhh!!! Your total score is ${totalScore} which is less than 10. Try once again!!`);
+                alert(`O Ohhh!!! Your total score is ${totalScore} which is less than 10. Try once again!! Click on 3rd image.`);
                 document.getElementById("img-3").disabled = false;
                 RemainingDiceClick = 3;
                 totalScore = 0;
@@ -94,7 +103,8 @@ function diceClick() {
 
             } else {
                 document.getElementById('score').innerText = totalScore;
-                alert(`Sorry Bad luck 🥹🥹🥹!!! Your total score is ${totalScore} which is not greater than 10 & You have exausted all your attempts. Please Start from begining!!!!`);
+                msg.classList.remove('hidden');
+                msg.innerHTML = `Sorry Bad luck 🥹🥹🥹!!! Your total score is ${totalScore} which is not greater than 10 & You have exausted all your attempts. Please Start from begining!!!!`
                 document.getElementById('diceWthScore').classList.add('hidden');
                 document.getElementById('tryAgainImage').classList.remove('hidden');
             }
@@ -111,5 +121,6 @@ function generateCouponCodeFunction() {
         coupon += Math.floor(Math.random() * 10);
     }
     document.getElementById('congratsImage').classList.remove('hidden');
-    alert(`Congratulations 🎉🎉🎊🎊😊😊!!! You have successfully generated a coupon. Your coupon code is ${coupon}`)
+    msg.classList.remove('hidden');
+    msg.innerHTML = `Congratulations 🎉🎉🎊🎊😊😊!!! You have successfully generated a coupon. Your coupon code is ${coupon}`
 }
